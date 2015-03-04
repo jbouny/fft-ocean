78dffdf595f43219549814420d1f4d6c5a82e1b9
Jérémy Bouny <jbouny@gmail.com>
2015-03-04 11:26:51 +0100

    Move final ocean shader rendering into OceanShader.js

---
+++ js/shaders/OceanShader.js
@@ -0,0 +1,84 @@
+﻿/**
+ * @author jbouny / https://github.com/fft-ocean
+ */
+ 
+ 
+THREE.ShaderLib['ocean_main'] = {
+	uniforms: THREE.UniformsLib[ "oceanfft" ],
+	vertexShader: [
+		'precision highp float;',
+		
+		'varying vec3 vWorldPosition;',
+		'varying vec4 vReflectCoordinates;',
+
+		'uniform mat4 u_mirrorMatrix;',
+		
+		THREE.ShaderChunk[ "screenplane_pars_vertex" ],
+		THREE.ShaderChunk[ "oceanfft_pars_vertex" ],
+
+		'void main (void) {',
+			THREE.ShaderChunk[ "screenplane_vertex" ],
+			
+			'vec4 worldPosition = screenPlaneWorldPosition;',
+			
+			THREE.ShaderChunk[ "oceanfft_vertex" ],
+			
+			'vWorldPosition = oceanfftWorldPosition.xyz;',
+			'vReflectCoordinates = u_mirrorMatrix * oceanfftWorldPosition;',
+			
+			'gl_Position = projectionMatrix * modelViewMatrix * oceanfftWorldPosition;',
+		'}'
+	].join('\n'),
+	fragmentShader: [
+		'precision highp float;',
+
+		'varying vec3 vWorldPosition;',
+		'varying vec4 vReflectCoordinates;',
+
+		'uniform sampler2D u_reflection;',
+		'uniform sampler2D u_normalMap;',
+		'uniform vec3 u_oceanColor;',
+		'uniform vec3 u_sunDirection;',
+		'uniform float u_exposure;',
+
+		'vec3 hdr (vec3 color, float exposure) {',
+			'return 1.0 - exp(-color * exposure);',
+		'}',
+		
+		THREE.ShaderChunk["screenplane_pars_fragment"],
+
+		'void main (void) {',
+			'vec3 normal = texture2D( u_normalMap, vWorldPosition.xz * 0.003 ).rgb;',
+			'vec3 view = normalize( vCamPosition - vWorldPosition );',
+			
+			// Compute the specular factor
+			'vec3 reflection = normalize( reflect( -u_sunDirection, normal ) );',
+			'float specularFactor = pow( max( 0.0, dot( view, reflection ) ), 200.0 ) * 20.0;',
+			
+			// Compute a ratio determined by the distance from the camera
+			'float distanceRatio = min( 1.0, log( 1.0 / length( vCamPosition - vWorldPosition ) * 3000.0 + 1.0 ) );',
+			'distanceRatio *= distanceRatio;',
+			'distanceRatio = distanceRatio * 0.9 + 0.1;',
+			
+			// Smooth the normal follwing the distance
+			'normal *= distanceRatio;',
+			
+			// Compute the fresnel ratio
+			'float fresnel = pow( 1.0 - dot( normal, view ), 2.0 );',
+		
+			// Get reflection color
+			'vec3 distortion = distanceRatio * 80.0 * ( normal - vec3( 0.0, 1.0, 0 ) ) * vec3( 1.0, 0.0, 0.2 );',	
+			'vec3 reflectionColor = texture2DProj( u_reflection, vReflectCoordinates.xyz + distortion ).xyz;',
+			
+			// Compute the sky reflection and the water color
+			'float skyFactor = fresnel * 10.0;',
+			'vec3 waterColor = ( 1.0 - fresnel ) * u_oceanColor;',
+			
+			// Compute the final color
+			'vec3 color = ( skyFactor + specularFactor + waterColor ) * reflectionColor + waterColor * 0.5 ;',
+			'color = hdr( color, u_exposure );',
+
+			'gl_FragColor = vec4( color, 1.0 );',
+		'}'
+	].join('\n')
+};
